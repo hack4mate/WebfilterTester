814521d8811cbd197d6b9d6e9f5214a46efc7a47
noah <noah@settmacher.de>
2024-05-23 22:47:32 +0200

    Changed the input from a txt file to a csv file. the same counts for the output file. now you can see all the sites checked in the output and what the status was

--- README.md
+++ README.md
@@ -3,13 +3,11 @@ For IT personnel who want to verify if their web filter settings actually work :
 
 ## Functionality
 
-To check your web filter, I've curated a file [Domains.txt](./Domains.txt) containing the most important website categories and domains.
+To check your web filter, I've curated a file [Domains.csv](./Domains.csv) containing the most important website categories and domains.
 
 When you start it as explained in [Usage](./README.md##Usage), you'll receive a file listing all the websites that were accessed.
 
 Depending on the user from which you started it, you shouldn't be able to access at least the domains listed in Critical. If you can access those, you really should fix your web filter ASAP.
-
-If you want a slightly fancier output, just save it as a Markdown file instead of a .txt file. Due to the categories being formatted with "#", they will be formatted as titles and subtitles.
 
 If you like this little script i would be happy if you could leave a Star
 
@@ -18,14 +16,14 @@ Thank you and have fun :)
 ## Usage:
 
 ```PowerShell
-./WebFilterTester.ps1 /Input/Path/With/Domains.txt /Output/File.txt
+./WebFilterTester.ps1 /Input/Path/With/Domains.csv /Output/File.csv
 
 ```
 
 ### Or just copy this command and run it ;) :
 
 ```PowerShell
-wget https://raw.githubusercontent.com/hack4mate/WebfilterTester/main/Domains.txt -OutFile Domains.txt;
+wget https://raw.githubusercontent.com/hack4mate/WebfilterTester/main/Domains.csv -OutFile Domains.csv;
 IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/hack4mate/WebfilterTester/main/WebFilterTester.ps1');
-WebFilterTest ./Domains.txt ./Outfile.txt
+WebFilterTest ./Domains.csv ./Outfile.csv
 ```
--- WebFilterTester.ps1
+++ WebFilterTester.ps1
@@ -1,80 +1,78 @@
 function WebFilterTest {
     [CmdletBinding()]
 
-param (
-    [string]$inputFilePath,
-    [string]$outputFilePath
-)
+    param (
+        [string]$inputFilePath,
+        [string]$outputFilePath
+    )
 
-# my logo 
-Write-Host "                                    011"
-Write-Host "                                  110  "
-Write-Host "                                110    "
-Write-Host "                             11011     "
-Write-Host "                            1001       "
-Write-Host "                          1100         "
-Write-Host "        1011001101110111011001         "
-Write-Host "      1011:::...        1011011        "
-Write-Host "     100::::::::....  1100   100       "
-Write-Host "    10111011101110111011101110111      "
-Write-Host "   00100000010011110111011100 1001   "
-Write-Host "  001000000100000101110111011  1001   "
-Write-Host " 10111 1011 100 10111011  100 1 1011   "
-Write-Host "110111 1011 100 100 1011   00 1 10111  "
-Write-Host "110111      100 100 1011 1   00 10111  "
-Write-Host "110111 1011 100       00 00  00 11011  "
-Write-Host " 10111 1011 1011100 1011 101110 01011  "
-Write-Host " 11000 1000 1011100 1011 10111011100   "
-Write-Host "   0010000010111011 101110111011001    "
-Write-Host "    010010001011101110111011111101     "
-Write-Host "      10111011101110111011101111       "
-Write-Host "        1110111011101110111011         "
-Write-Host "            01101110001000             "
-Write-Host "                                       "
-Write-Host " ----------------------------------- "
-Write-Host "|        Test your Webfilter        |"
-Write-Host "|        Twitter @ Hack4Mate        |"
-Write-Host " ----------------------------------- "
-Write-Host "                                      "
+    # my logo 
+    Write-Host "                                    011"
+    Write-Host "                                  110  "
+    Write-Host "                                110    "
+    Write-Host "                             11011     "
+    Write-Host "                            1001       "
+    Write-Host "                          1100         "
+    Write-Host "        1011001101110111011001         "
+    Write-Host "      1011:::...        1011011        "
+    Write-Host "     100::::::::....  1100   100       "
+    Write-Host "    10111011101110111011101110111      "
+    Write-Host "   00100000010011110111011100 1001   "
+    Write-Host "  001000000100000101110111011  1001   "
+    Write-Host " 10111 1011 100 10111011  100 1 1011   "
+    Write-Host "110111 1011 100 100 1011   00 1 10111  "
+    Write-Host "110111      100 100 1011 1   00 10111  "
+    Write-Host "110111 1011 100       00 00  00 11011  "
+    Write-Host " 10111 1011 1011100 1011 101110 01011  "
+    Write-Host " 11000 1000 1011100 1011 10111011100   "
+    Write-Host "   0010000010111011 101110111011001    "
+    Write-Host "    010010001011101110111011111101     "
+    Write-Host "      10111011101110111011101111       "
+    Write-Host "        1110111011101110111011         "
+    Write-Host "            01101110001000             "
+    Write-Host "                                       "
+    Write-Host " ----------------------------------- "
+    Write-Host "|        Test your Webfilter        |"
+    Write-Host "|        Twitter @ Hack4Mate        |"
+    Write-Host " ----------------------------------- "
+    Write-Host "                                      "
 
-
-# checks for input file
-if (-not (Test-Path $inputFilePath)) {
-    Write-Output "Input File '$inputFilePath' doesen't exist."
-    exit
-}
-
-# Open output file to write
-$outputFile = [System.IO.File]::CreateText($outputFilePath)
-
-$inputList = Get-Content -Path $inputFilePath
-foreach ($line in $inputList) {
-    # Checks for title
-    Start-Sleep -Seconds 2
-    if ($line.Contains("#")) {
-        $line | Add-Content -Path $outputFilePath
-        Write-Output "Checking: $line"
-    } 
-    elseif ($line -eq "") {
-        $line | Add-Content -Path $outputFilePath
-    } 
-    # Checks site
-    else {
-        try {
-            $HTTP_Request = Invoke-WebRequest $line
-            if ($HTTP_Request.StatusCode -eq 200) {
-                $line | Add-Content -Path $outputFilePath
-            }
-        } catch {
-            Write-Output "Site blocked or down: $line"
-        }
+    # checks for input file
+    if (-not (Test-Path $inputFilePath)) {
+        Write-Output "Input File '$inputFilePath' doesn't exist."
+        exit
     }
+
+    # Read input CSV
+    $inputList = Import-Csv -Path $inputFilePath -Delimiter ';'
+    
+    # Open output file to write
+    $outputFile = [System.IO.StreamWriter]::new($outputFilePath)
+    "Criticality;Category;Domain;Reachable" | Add-Content -Path $outputFilePath
+    
+    foreach ($entry in $inputList) {
+        $criticality = $entry.criticality
+        $category = $entry.category
+        $domain = $entry.domain
+
+        # Check if site is reachable
+        try {
+            $HTTP_Request = Invoke-WebRequest $domain
+            if ($HTTP_Request.StatusCode -eq 200) {
+                $reachable = "Not Blocked"
+                Write-Output "Can connect to site: $domain"
+
+            } 
+        } catch {
+            $reachable = "Blocked"
+            Write-Output "Site blocked: $domain"
+        }
+        # Write result to output file
+        "$criticality;$category;$domain;$reachable" | Add-Content -Path $outputFilePath
+    }
+
+    # Closes file.
+    $outputFile.Close()
+
+    Write-Output "Done - Results written to: $outputFilePath"
 }
-
-# Closes file.
-$outputFile.Close()
-
-Write-Output "Done - File with Sites that aren't blocked: $outputFilePath"
-
-}
-
